163384a2c4bef0777da0e933c9400030fd5b709f
Shakalyan <vdovit@mail.ru>
2022-09-30 16:09:19 +0300

    Initial commit

---
+++ src/main/java/com/sns/sns_bff/SnsBffApplication.java
@@ -0,0 +1,13 @@
+package com.sns.sns_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sns/sns_bff/SnsBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sns.sns_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
