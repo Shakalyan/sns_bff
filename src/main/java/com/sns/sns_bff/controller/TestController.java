bbdacd030fcf9848c6a9d8359b835f596c23f946
Shakalyan <vdovit@mail.ru>
2022-11-18 15:46:48 +0300

    test sing up commit

---
+++ src/main/java/com/sns/sns_bff/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.sns.sns_bff.controller;
+
+import com.sns.sns_bff.snsApi.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+  @Autowired
+  private TestService testService;
+
+  @GetMapping
+  public String test() {
+    return testService.testApi();
+  }
+
+}
